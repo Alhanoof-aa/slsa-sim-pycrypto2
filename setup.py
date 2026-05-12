c15257838c01fe3cab801bd87ada3b27d270ab68
Alhanoof <aa1573@exeter.ac.uk>
2026-05-12 10:08:54 +0100

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup, find_packages setup(
+    name="pycrypto2-sim", 
+    version="0.1.0", 
+    description="PhD SLSA research simulation - pycrypto2",
+    packages=find_packages(),
+    python_requires=">=3.8",
+)
